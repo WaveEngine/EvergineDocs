c2efb6cff270f1aba347180af7fe866bd3cabbf3
Sergio Escalada <sescalada@gmail.com>
2024-07-02 11:34:05 +0200

    Trying to fix API not being generated

--- build/buildDoc.ps1
+++ build/buildDoc.ps1
@@ -10,6 +10,10 @@ if(-Not ($docfxInstalled))
 	(New-Object Net.WebClient).DownloadFile('https://github.com/dotnet/docfx/releases/download/v2.76.0/docfx-win-x64-v2.76.0.zip', "$toolsDir\Tools\docfx.zip");(new-object -com shell.application).namespace("$toolsDir\Tools").CopyHere((new-object -com shell.application).namespace("$toolsDir\Tools\docfx.zip").Items(),16)
 }
 
+Tools\docfx ..\docfx_metadata_net8-android.json
+Tools\docfx ..\docfx_metadata_net8-ios.json
+Tools\docfx ..\docfx_metadata_net8-windows.json
+Tools\docfx ..\docfx_metadata_net8.json
 Tools\docfx ..\docfx.json
 
 Pop-Location
